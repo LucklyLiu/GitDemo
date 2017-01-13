7ed00d313985368fb9b3b5c3a9c31e43e0fecd8a
LucklyLiu <380089760@qq.com>
2017-01-13 11:58:24 +0800

    第一次更新

--- app/src/main/java/com/news/newsmainactivity/gitdemo/MainActivity.java
+++ app/src/main/java/com/news/newsmainactivity/gitdemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.news.newsmainactivity.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // TODO: 2017/1/13  
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
+        // TODO: 2017/1/13
     }
 }
