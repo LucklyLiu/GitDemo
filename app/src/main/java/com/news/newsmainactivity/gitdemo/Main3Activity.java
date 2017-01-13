d9c1ea8b2a70d6bb7cf50cabb7efd247c688138d
LucklyLiu <380089760@qq.com>
2017-01-13 14:36:50 +0800

    第一次更新

---
+++ app/src/main/java/com/news/newsmainactivity/gitdemo/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.news.newsmainactivity.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        // TODO: 2017/1/13  
+        // TODO: 2017/1/13  
+    }
+}
